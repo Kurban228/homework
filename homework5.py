0a57ece67522c0c59e8764f3719dcf0671cfd42f
Kurban228 <marat.gadzhiyev.01@bk.ru>
2024-07-22 16:09:18 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,6 @@
+immutable_var = (1, 2, 'a', 'b')
+print("Immutable tuple:", immutable_var)
+mutable_list = [1, 2, 'a', 'b']
+mutable_list[2] = 'Modified'
+mutable_list.append('New Element')
+print("Mutable list:", mutable_list)
