d09693ada7b3878fd6968642a89c9395cf82bff8
Kurban228 <marat.gadzhiyev.01@bk.ru>
2024-07-22 15:54:01 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,4 @@
+total_tasks = 12
+hours_spent = 1.5
+course = 'Python'
+print('Курс:',course,', ''всего задач:',total_tasks,', ''затрачено часов:',hours_spent,', ''среднее время выполнения',hours_spent/total_tasks )
