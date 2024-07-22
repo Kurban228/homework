aff558210f377d22aebf83a5d41e475515b3c628
Kurban228 <marat.gadzhiyev.01@bk.ru>
2024-07-22 16:02:47 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,6 @@
+my_string = input('Введите любую строку: ')
+print('в введенной строке ', len(my_string), 'символов')
+print(my_string.upper())
+print(my_string.lower())
+my_string = my_string.replace(' ', '')
+print(my_string)
